217b2c0aee9989e3b4c87351a4e78a2b517090be
mahdi aghajani <mma137421@gmail.com>
2016-08-31 20:04:11 +0430

    Adds play and pause state to player

--- test/app/view1/view1.js
+++ test/app/view1/view1.js
@@ -64,47 +64,22 @@ app.controller('View1Ctrl', function ($scope, $http, md5, $interval) {
                     player.src({"type": "application/x-mpegURL", "src": url.url, "withCredentials": "true"});
                     player.play();
                     //player.pause() ;
-                    player.on('pause', function () {
-                        player.src({"type": "application/x-mpegURL", "src": '', "withCredentials": "true"});
-                        var s = '{ "contentId": 8 , "type":"TV" , "locale":"fa-IR" ' +
-                            ', "profileGuid" : "' + profileGuid + '" , "sessionId" : "' + sessionID + '"}';
-                        digest = createDigest("streams/stop", s);
-                        $http({
-                            url: "https://tv.aionet.ir/Catherine/api/5.4/json/7743461522282941752/" + digest +
-                            "/client/streams/stop",
-                            data: s,
-                            method: "POST",
-                            headers: {"Content-Type": "application/json"}
-                        }).then(function (response) {
-                            console.log("response ok pause");
-                            $interval.cancel(promise);
-                        });
-                        // Now the issue is that we need to hide it again if we start playing
-                        // So every time we do this, we can create a one-time listener for play events.
-                    });
                     player.on('play', function () {
                         var source = player.currentSrc();
                         if (source == '') {
-                            digest = createDigest("channels/linear/getUrl", '{ "channelType":"TV" , "playbackType":"LIVE" , "channelId": 8 , "locale":"fa-IR" , "protocols" ' +
+                            digest = createDigest("channels/linear/getUrl", '{ "channelType":"TV" , "playbackType":"PLTV" , "channelId": 8 , "locale":"fa-IR" , "protocols" ' +
                                 ': ["HLS"] , "pictureTypes" : ["_2D"] , "audioFormats" : ["AAC"] , "delay" : 0 , "profileGuid" : "'
                                 + profileGuid + '" , "sessionId" : "' + sessionID + '"}');
                             $http({
                                 url: "https://tv.aionet.ir/Catherine/api/5.4/json/7743461522282941752/" + digest +
                                 "/client/channels/linear/getUrl",
-                                data: '{ "channelType":"TV" , "playbackType":"LIVE" , "channelId": 8 , "locale":"fa-IR" , "protocols" ' +
+                                data: '{ "channelType":"TV" , "playbackType":"PLTV" , "channelId": 8 , "locale":"fa-IR" , "protocols" ' +
                                 ': ["HLS"] , "pictureTypes" : ["_2D"] , "audioFormats" : ["AAC"] , "delay" : 0 , "profileGuid" : "'
                                 + profileGuid + '" , "sessionId" : "' + sessionID + '"}',
                                 method: "POST",
                                 headers: {"Content-Type": "application/json"}
                             }).then(function (response) {
                                 url = response.data;
-                                console.log("recognized");
-                                player.src({
-                                    "type": "application/x-mpegURL",
-                                    "src": url.url,
-                                    "withCredentials": "true"
-                                });
-                                player.play();
                                 console.log("response ok");
                                 digest = createDigest("ping", '{"contentId" : 8 , "type" : "TV" , "delay" : 0 , ' +
                                     '"locale":"fa-IR","sessionId":' + sessionID.toString() + '}');
@@ -119,8 +94,35 @@ app.controller('View1Ctrl', function ($scope, $http, md5, $interval) {
                                         $scope.ping = response.data;
                                     });
                                 }, platformInformation.pingRepeatFrom);
+                                player.src({
+                                    "type": "application/x-mpegURL",
+                                    "src": url.url,
+                                    "withCredentials": "true"
+                                });
+                                player.play();
                             });
                         }
+
+                        // Now the issue is that we need to hide it again if we start playing
+                        // So every time we do this, we can create a one-time listener for play events.
+
+                    });
+
+                    player.on('pause', function () {
+                        var s = '{ "contentId": 8 , "type":"TV" , "locale":"fa-IR" ' +
+                            ', "profileGuid" : "' + profileGuid + '" , "sessionId" : "' + sessionID + '"}';
+                        digest = createDigest("streams/stop", s);
+                        $http({
+                            url: "https://tv.aionet.ir/Catherine/api/5.4/json/7743461522282941752/" + digest +
+                            "/client/streams/stop",
+                            data: s,
+                            method: "POST",
+                            headers: {"Content-Type": "application/json"}
+                        }).then(function (response) {
+                            // player.src({"type": "application/x-mpegURL", "src": '', "withCredentials": "true"});
+                            console.log("response ok pause");
+                            $interval.cancel(promise);
+                        });
                     });
                 });
             });
